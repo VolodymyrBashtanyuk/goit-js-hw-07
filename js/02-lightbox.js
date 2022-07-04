ff3987195dc4fae36050fd415ea5f53bb5bc6dcb
VolodymyrBashtanyuk <vladimirbashtanuk@gmail.com>
2022-07-04 15:37:59 +0300

    task-02 complete

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+const galleryItemList = document.querySelector('.gallery');
+const galleryList = createGalleryList(galleryItems);
+galleryItemList.insertAdjacentHTML('beforeend', galleryList)
+
+// console.log(galleryList)
+function createGalleryList(itemList) {
+    return itemList.map(({ preview, original, description }) => {
+        return `<a class="gallery__link" href="${original}">
+    <img
+      loading = 'lazy'
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      title = "${description}"
+    />
+    </a>`;
+    }).join(' ');
+};
+
+
+
+let galleryShowModal = new SimpleLightbox('.gallery a');
+galleryShowModal.on('show.simplelightbox', onGalleryShowModal);
+
+function onGalleryShowModal(event) {
+    event.preventDefault();
+}
