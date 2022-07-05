88844927e03ec0bb43f65f02c1db061980dde8b3
VolodymyrBashtanyuk <vladimirbashtanuk@gmail.com>
2022-07-05 20:09:37 +0300

    task-02 correction done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,14 +1,11 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// console.log(galleryItems);
-
-
 const galleryItemList = document.querySelector('.gallery');
 const galleryList = createGalleryList(galleryItems);
 galleryItemList.insertAdjacentHTML('beforeend', galleryList)
 
-// console.log(galleryList)
+
 function createGalleryList(itemList) {
     return itemList.map(({ preview, original, description }) => {
         return `<a class="gallery__link" href="${original}">
@@ -16,19 +13,11 @@ function createGalleryList(itemList) {
       loading = 'lazy'
       class="gallery__image"
       src="${preview}"
-      data-source="${original}"
       alt="${description}"
-      title = "${description}"
     />
     </a>`;
     }).join(' ');
 };
 
+new SimpleLightbox( '.gallery a', {captionsData: 'alt' , captionDelay: 250 });
 
-
-let galleryShowModal = new SimpleLightbox('.gallery a');
-galleryShowModal.on('show.simplelightbox', onGalleryShowModal);
-
-function onGalleryShowModal(event) {
-    event.preventDefault();
-}
